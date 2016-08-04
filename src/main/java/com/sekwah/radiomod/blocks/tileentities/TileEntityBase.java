fff2bd25b481c1bc76d3809224b39b228e64c41a
Alastair <alastair.hawkes@gmail.com>
2016-08-04 23:48:39 +0000

    Updated the nbt tile entity data to remove an error

--- src/main/java/com/sekwah/radiomod/blocks/tileentities/TileEntityBase.java
+++ src/main/java/com/sekwah/radiomod/blocks/tileentities/TileEntityBase.java
@@ -3,22 +3,35 @@ package com.sekwah.radiomod.blocks.tileentities;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.Packet;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ITickable;
 
 /**
  * Created by on 04/08/2016.
  *
  * @author sekwah41
  */
-public class TileEntityBase extends TileEntity {
-    @Override
-    public NBTTagCompound writeToNBT(NBTTagCompound par1) {
-        super.writeToNBT(par1);
-        return par1;
-    }
+public class TileEntityBase extends TileEntity implements ITickable {
+
 
     @Override
-    public void readFromNBT(NBTTagCompound par1) {
-        super.readFromNBT(par1);
+    public void update() {
+
     }
+
+    public void readFromNBT(NBTTagCompound compound) {
+        super.readFromNBT(compound);
+    }
+    public NBTTagCompound writeToNBT(NBTTagCompound compound)
+    {
+        super.writeToNBT(compound);
+
+        /**
+         * Check chest for largs nbt writing {@link net.minecraft.tileentity.TileEntityChest}
+         * Need to start using links more. They are pretty useful :D
+         */
+
+        return compound;
+    }
+
 
 }
