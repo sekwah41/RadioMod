1a6618954d55d5a76e2cf15f820c70538d7b4c11
Alastair <alastair.hawkes@gmail.com>
2016-08-04 23:35:04 +0000

    Fixed block rendering

--- src/main/java/com/sekwah/radiomod/blocks/TestRadio.java
+++ src/main/java/com/sekwah/radiomod/blocks/TestRadio.java
@@ -13,6 +13,7 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumBlockRenderType;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.AxisAlignedBB;
@@ -28,18 +29,21 @@ import net.minecraft.world.World;
 public class TestRadio extends Block implements ITileEntityProvider {
 
     private final AxisAlignedBB BOUNDING_BOX;
-    
+
     public static final int RUNSTATE_OFF = -1;
     public static final int RUNSTATE_BOOTINGUP = 0;
     public static final int RUNSTATE_ON = 1;
-    
+
     public TestRadio() {
         super(Material.GLASS);
-        BOUNDING_BOX = new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 1.0D, 1.0D);
+        BOUNDING_BOX = new AxisAlignedBB(0.3D, 0.0D, 0.3D, 0.7D, 0.7D, 0.7D);
     }
 
-    @Override
-    public TileEntity createNewTileEntity(World worldIn, int meta) {
+    /**
+     * Returns a new instance of a block's tile entity class. Called on placing the block.
+     */
+    public TileEntity createNewTileEntity(World par1World, int meta) {
+        RadioMod.logger.info("Test");
         return new TileEntityBase();
     }
 
@@ -47,11 +51,33 @@ public class TestRadio extends Block implements ITileEntityProvider {
     {
         return BOUNDING_BOX;
     }
-    
+
     public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, @Nullable ItemStack heldItem, EnumFacing side, float hitX, float hitY, float hitZ)
     {
-    	playerIn.openGui(RadioMod.instance, GuiHandlerRadio.GUIID_COMPUTER, worldIn, (int)hitX, (int)hitY, (int)hitZ);
-    	
+        playerIn.openGui(RadioMod.instance, GuiHandlerRadio.GUIID_COMPUTER, worldIn, (int)hitX, (int)hitY, (int)hitZ);
+
         return true;
+    }
+
+    /**
+     * Used to determine ambient occlusion and culling when rebuilding chunks for render
+     */
+    public boolean isOpaqueCube(IBlockState state)
+    {
+        return false;
+    }
+
+    public boolean isFullCube(IBlockState state)
+    {
+        return false;
+    }
+
+    /**
+     * The type of render function called. 3 for standard block models, 2 for TESR's, 1 for liquids, -1 is no render
+     */
+    @Deprecated
+    public EnumBlockRenderType getRenderType(IBlockState state)
+    {
+        return EnumBlockRenderType.ENTITYBLOCK_ANIMATED;
     }
 }
